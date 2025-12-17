e8358f9ffe595a3f2f5fc8426daa85e384a06369
999s <123440999+mysbryce@users.noreply.github.com>
2025-12-17 09:23:40 +0700

    feat: example for noone

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,45 @@
 import { cn } from '@/utils/cn'
+import { useState } from 'react'
+import { useEvent } from './utils/useEvent'
+
+interface Property {
+  firstname: string
+  lastname: string
+}
 
 export default function App() {
-  return <div className={cn('bg-black p-4')}></div>
+  const [show, setShow] = useState(false)
+  const [property, setProperty] = useState<Property>({ firstname: '', lastname: '' })
+
+  /**
+   * Call to this by
+    SendNUIMessage({
+      type = 'set-show',
+      data = true | false
+    })
+   */
+  useEvent<boolean>('set-show', (show) => setShow(show))
+
+  /**
+   * Call to this by
+    SendNUIMessage({
+      type = 'set-property',
+      data = {
+        firstname = 'target-first-name',
+        lastname = 'target-last-name'
+      }
+    })
+   */
+  useEvent<Property>('set-property', (property) => setProperty(property))
+
+  return (
+    <div
+      className={cn('bg-black p-4 transition-all duration-200', {
+        '!opacity-0': !show,
+      })}
+    >
+      {property.firstname}
+      {property.lastname}
+    </div>
+  )
 }
